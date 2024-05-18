de3464cb8625b97cd83acaf8ea66f96d58bfe10b
Vitor <vitormilanezi@gmail.com>
2024-05-18 15:43:17 -0300

    atualizacao

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
     const firebaseConfig = {
-        apiKey: "AIzaSyB5yN8aSHdWMXLPghJhKj81U_r-NqXI61o",
-        authDomain: "sitepersonal-32eb9.firebaseapp.com",
-        projectId: "sitepersonal-32eb9",
-        storageBucket: "sitepersonal-32eb9.appspot.com",
-        messagingSenderId: "140124961554",
-        appId: "1:140124961554:web:8cff7d3d2258ecf04d51cb"
-    };
+        apiKey: "AIzaSyDPdvvr0BehxgAX-0gl3GjbeZf_FNGkDbw",
+        authDomain: "sitepersonal-28a8f.firebaseapp.com",
+        projectId: "sitepersonal-28a8f",
+        storageBucket: "sitepersonal-28a8f.appspot.com",
+        messagingSenderId: "551007212453",
+        appId: "1:551007212453:web:4c72b19f4cc9402e684e4c"
+      };
 
     firebase.initializeApp(firebaseConfig);
 
     function findTransactions() {
-        firebase.firestore().collection('cadastro').get().then(snapshot => {
+        firebase.firestore().collection('usuarios').get().then(snapshot => {
             snapshot.docs.forEach(doc => {
                 console.log(doc.data()); // Exibe os dados do documento
             });
@@ -29,9 +29,7 @@ document.addEventListener('DOMContentLoaded', function () {
         const celular = document.getElementById('number').value;
         const senha = document.getElementById('password').value;
         const confirmarSenha = document.getElementById('confirmPassword').value;
-        const genero = document.querySelector('input[name="gender"]:checked').value;
-
-        // Validação dos campos (se necessário)
+        const genero = document.querySelector('input[name="gender"]:checked').value;        
 
         // Verifica se a senha e a confirmação de senha são iguais
         if (senha !== confirmarSenha) {
@@ -40,7 +38,7 @@ document.addEventListener('DOMContentLoaded', function () {
         }
 
         // Adiciona o usuário ao Firestore
-        firebase.firestore().collection('cadastro').add({
+        firebase.firestore().collection('usuarios').add({
             nomeCompleto: nomeCompleto,
             email: email,
             celular: celular,
@@ -63,67 +61,5 @@ document.addEventListener('DOMContentLoaded', function () {
             });
 
     }
-
-
-    if (window.location.pathname.includes("usuarios.html")) {
-        const db = firebase.firestore();
-        const usuariosRef = db.collection("cadastro");
-
-        usuariosRef.onSnapshot(function (snapshot) {
-            // Limpa o corpo da tabela
-
-            const usuariosTable = document.querySelector('#usuarios');
-            console.log(usuariosTable);
-            if (usuariosTable) {
-                usuariosTable.innerHTML = '';
-                // Itera sobre os documentos recuperados do Firestore
-                snapshot.forEach(function (doc) {
-                    var dado = doc.data();
-                    var newRow = document.querySelector('#usuarios').insertRow();
-                    newRow.innerHTML = '<td>' + dado.nomeCompleto + '</td>' + '<td>' + dado.email + '</td>' + '<td>' + dado.celular + '</td>';
-
-                    // Adiciona os ícones clicáveis na célula "Ações"
-                    var actionsCell = newRow.insertCell();
-                    var editIcon = document.createElement('img');
-                    editIcon.src = './assets/img/edit.png';
-                    editIcon.title = 'Editar';
-                    editIcon.alt = 'Editar';
-                    editIcon.classList.add('iconEdit');
-                    editIcon.style.cursor = 'pointer';
-                    actionsCell.appendChild(editIcon);
-
-                    var deleteIcon = document.createElement('img');
-                    deleteIcon.src = './assets/img/delete.png';
-                    deleteIcon.title = 'Excluir';
-                    deleteIcon.alt = 'Excluir';
-                    deleteIcon.classList.add('iconDelete');
-                    deleteIcon.style.cursor = 'pointer';
-                    actionsCell.appendChild(deleteIcon);
-
-                    // Armazena o ID do usuário como um atributo data na linha da tabela
-                    newRow.setAttribute('data-user-id', doc.id);
-
-
-                    editIcon.addEventListener('click', function () {
-                        console.log('Editar usuário')
-                    });
-
-                    deleteIcon.addEventListener('click', function () {
-                        // Pega o ID do usuário da linha clicada na tabela
-                        var userId = newRow.getAttribute('data-user-id');
-                        // Exclui o usuário do Firestore
-                        db.collection("cadastro").doc(userId).delete().then(function () {
-                            console.log("Usuário excluído com sucesso!");
-                            // Remove a linha correspondente da tabela HTML
-                            newRow.remove();
-                        }).catch(function (error) {
-                            console.error("Erro ao excluir usuário: ", error);
-                        });
-                    });
-                });
-            } else {
-                console.error("Elemento '#usuarios' não encontrado no DOM.");
-            }
-        });
-    }
+    
 });
